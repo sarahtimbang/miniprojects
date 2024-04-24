c1c87af79917e55c7c6f01ab77af6f27da0c0cd9
SarahTimbang <sarah.timbang@gmail.com>
2024-04-23 21:48:21 -0400

    day 5

---
+++ 6 - Javascript/looping-statement.js
@@ -0,0 +1,20 @@
+//For loop
+for  (let i = 1; i <= 5; i++) {
+    console.log('Count:', i);
+}
+
+
+// While loop
+let count = 1;
+while (count <= 5) {
+    console.log('Processing...')
+    count++
+    console.log('While count:', count);
+}
+
+// do-while loop
+let counter = 1;
+do {
+    console.log('Do-while count:', counter)
+    counter++
+} while (counter <= 5);
