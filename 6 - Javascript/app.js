e5475b0a3e5c200838875610ff2262f98027b9ee
SarahTimbang <sarah.timbang@gmail.com>
2024-04-23 21:49:22 -0400

    day 5

---
+++ 6 - Javascript/app.js
@@ -0,0 +1,23 @@
+// Defining a function - Creating a function
+function greet() {
+    console.log('Hello, welcome to the world of functions!');
+    console.log('Yehey');
+}
+
+// Calling a function - Use a function
+greet();
+
+
+
+console.log(sum(150,199));
+// Defining functions with parameters
+function sum(num1, num2) {
+    console.log('Calculating thhe value...')
+    return num1 + num2;
+    console.log('Calculation is done!');
+}
+
+
+// Calling a function with parameters
+console.log(sum(150, 199));
+//The values that are being pass to a parameter is called an arguments
